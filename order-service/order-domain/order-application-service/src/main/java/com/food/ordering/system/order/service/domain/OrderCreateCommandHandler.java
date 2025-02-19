4c596d8587f373671c1efd0638cf7768286bf639
Kiransing Bhat <bhatkiran74@gmail.com>
2025-02-19 17:03:53 +0530

    created CommandHandler

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderCreateCommandHandler.java
@@ -0,0 +1,12 @@
+package com.food.ordering.system.order.service.domain;
+
+import com.food.ordering.system.order.service.domain.dto.create.CreateOrderCommand;
+import com.food.ordering.system.order.service.domain.dto.create.CreateOrderResponse;
+
+public class OrderCreateCommandHandler {
+
+
+    public CreateOrderResponse createOrder(CreateOrderCommand createOrderCommand){
+
+    }
+}
---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderTrackCommandHandler.java
@@ -0,0 +1,14 @@
+package com.food.ordering.system.order.service.domain;
+
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderQuery;
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderResponse;
+
+public class OrderTrackCommandHandler {
+
+
+
+
+    public TrackOrderResponse trackOrder(TrackOrderQuery trackOrderQuery){
+
+    }
+}
